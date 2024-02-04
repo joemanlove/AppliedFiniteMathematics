c9b896be2d300d5245a80305887b3763b69a97a7
joemanlove <33290590+joemanlove@users.noreply.github.com>
2024-02-03 16:05:24 -0800

    Moved to a section file structure
    using import package for nested file structure and relative imports.

---
+++ chapters/chapter01/makefolders.py
@@ -0,0 +1,22 @@
+"""Script to make all the section folders and blank tex files."""
+
+import os
+
+number_of_sections = 5
+
+# Define the base folder name
+base_folder_name = "section"
+DIRECTORY_PATH = os.path.dirname(__file__)
+PARENT_DIRECTORY = os.path.basename(DIRECTORY_PATH)
+
+# Create 12 folders
+for i in range(number_of_sections):
+    # Use f-strings to format the folder name with leading zeros
+    folder_name = f"{base_folder_name}{i:02}"
+    os.makedirs(folder_name)
+
+    # Create a blank .tex file in each folder
+    with open(os.path.join(folder_name, f"{PARENT_DIRECTORY}{folder_name}.tex"), "w") as tex_file:
+        pass
+
+print("Folders and blank .tex files created successfully!")
